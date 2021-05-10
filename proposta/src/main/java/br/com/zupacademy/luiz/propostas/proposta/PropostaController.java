2e6cf23bf9c90490a948454ad21a7a9b82f0dabe
luiz.moraes <luiz.moraes@host.docker.internal>
2021-05-10 15:25:03 -0300

    Não pode haver proposta duplicada

--- proposta/src/main/java/br/com/zupacademy/luiz/propostas/proposta/PropostaController.java
+++ proposta/src/main/java/br/com/zupacademy/luiz/propostas/proposta/PropostaController.java
@@ -1,6 +1,7 @@
 package br.com.zupacademy.luiz.propostas.proposta;
 
 import java.net.URI;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.support.Repositories;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,9 +28,15 @@ public class PropostaController {
 	private final Logger logger = LoggerFactory.getLogger(PropostaController.class);
 	
 	@PostMapping
-	public ResponseEntity<Proposta> cadastrar(@RequestBody @Valid PropostaRequest request, UriComponentsBuilder builder){
+	public ResponseEntity<?> cadastrar(@RequestBody @Valid PropostaRequest request, UriComponentsBuilder builder){
 		
 		Proposta novaProposta = request.tranformaProposta();
+		Optional<Proposta> props = propostaRepository.findByDocumento(novaProposta.getDocumento());
+		
+		if(props.isPresent()) {
+			 return ResponseEntity.status(422).body("Já existe uma proposta para este solicitante.");
+		}
+		
 				
 		URI enderecoUri = builder.path("proposta/{id}").build(novaProposta.getDocumento());
 		
--- proposta/src/main/java/br/com/zupacademy/luiz/propostas/proposta/PropostaRepository.java
+++ proposta/src/main/java/br/com/zupacademy/luiz/propostas/proposta/PropostaRepository.java
@@ -1,8 +1,12 @@
 package br.com.zupacademy.luiz.propostas.proposta;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface PropostaRepository extends CrudRepository<Proposta, Long>{
+
+	Optional<Proposta> findByDocumento(String documento);
 
 
 }
